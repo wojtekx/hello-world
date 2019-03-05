27dea889a9ed5527f72025043adc0549540ae434
wkogut93 <w.kogut@ideo.pl>
2019-03-05 15:52:32 +0100

    Create Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+     <div id="app">
+        <h2>Pogoda:</h2>
+        <div id="weather"></div>
+        <select v-model="city">
+            <option v-for="state in states" v-bind:key="state.id" :value="state.name">{{ state.name }}</option>
+        </select>
+        <button v-on:click="getData()">Szukaj</button>
+        
+    </div>
+</template>
+
+<script>
+  import states from "./list.json";
+export default {
+name: 'Header',
+ 
+  data() {
+                return {
+                   city: states[0].id,
+                   API: "f3c08ceff7f970ccc92f3aab10216c6b",
+                   states: states
+                };
+            },
+            methods:{
+                getData() {
+                    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.city}&units=metric&appid=${this.API}`)
+                    .then(response => response.json())
+                    .then(data => console.log(data)
+                    )
+                },
+            },
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
+
+ 
+
