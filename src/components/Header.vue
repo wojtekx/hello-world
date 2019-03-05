4c0d23ba2fe1c8b081c1af7b83f70ae457fad18a
wojtekx <36897622+wojtekx@users.noreply.github.com>
2019-03-05 20:14:33 +0100

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,42 +1,69 @@
 <template>
-     <div id="app">
-        <h2>Pogoda:</h2>
-        <div id="weather"></div>
-        <select v-model="city">
-            <option v-for="state in states" v-bind:key="state.id" :value="state.name">{{ state.name }}</option>
-        </select>
-        <button v-on:click="getData()">Szukaj</button>
-        
+  <div class="app">
+    <h2>Pogoda:</h2>
+    <div class="weather">
+      <h5>Miasto: {{cityName}}</h5>
+      <p>Temperatura: {{temp}}</p>
+      <p>Wilgotność: {{humidity}}</p>
+      <select v-model="city">
+        <option v-for="state in states" v-bind:key="state.id" :value="state.name">{{ state.name }}</option>
+      </select>
+      <button v-on:click="getData()">Szukaj</button>
     </div>
+    <div class="moreInfo" v-for="temp in moreTemp" :key="temp.dt">
+      <div>
+        <p>{{ temp.dt_txt.slice(11) }}</p>
+        <p>Temperatura: {{Math.round(temp.main.temp)}}°C</p>
+        <p>Wilgotność: {{Math.round(temp.main.humidity)}}%</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  import states from "./list.json";
+import states from "./list.json";
 export default {
-name: 'Header',
- 
+  name: "Header",
+
   data() {
-                return {
-                   city: states[0].id,
-                   API: "f3c08ceff7f970ccc92f3aab10216c6b",
-                   states: states
-                };
-            },
-            methods:{
-                getData() {
-                    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.city}&units=metric&appid=${this.API}`)
-                    .then(response => response.json())
-                    .then(data => console.log(data)
-                    )
-                },
-            },
-
-}
-
+    return {
+      city: states[0].id,
+      API: "f3c08ceff7f970ccc92f3aab10216c6b",
+      states: states,
+      temp: "",
+      humidity: "",
+      cityName: "",
+      moreTemp: ""
+    };
+  },
+  methods: {
+    getData() {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?q=${
+          this.city
+        }&units=metric&appid=${this.API}`
+      )
+        .then(response => response.json())
+        .then(data => {
+          this.temp = data.list[0].main.temp;
+          this.humidity = data.list[0].main.humidity;
+          this.cityName = data.city.name;
+          this.moreTemp = data.list;
+        });
+    }
+  }
+};
 </script>
 
 <style scoped>
-
+.moreInfo {
+  /* display: inline-flex; */
+  display: none;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100px;
+  flex-wrap: wrap;
+}
 </style>
 
  
