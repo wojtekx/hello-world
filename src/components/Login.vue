6a4b856b3409beb81d8913117eb3a2943512cb21
wkogut93 <w.kogut@ideo.pl>
2019-03-18 09:47:32 +0100

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,100 +1,77 @@
 <template>
-    <div class="container col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
-        <form id="form" v-on:submit.prevent="checkUsers()">
-            {{start()}}
-                <div class="form-group">
-                    <label for="inputLog">Login</label>
-                    <input type="text" class="form-control col-2" id="inputLog" placeholder="Enter login">
-                </div>
-                <div class="form-group">
-                    <label for="inputPass">Password</label>
-                    <input type="password" class="form-control col-2" id="inputPass" placeholder="Password">
-                </div>
-                <button type="submit" class="btn btn-secondary">Submit</button><br>
-                <span id="wrong" ></span>
-        </form>
+    <div class="login ">
+         <h3>Sign in</h3>
+         <input type="email" v-model="email" placeholder="Email"><br>
+         <input type="password" v-model="password" placeholder="Password"><br>
+         <button @click="login">Connection</button>
+         <p>You don't have an acount? You can  <router-link to="/SignUp">create one</router-link></p>
+        
     </div>    
 </template>
 
 <script>
-import '@/others/jquery-3.3.1.js'
+import firebase from 'firebase';
+import { all } from 'q';
 export default {
 name: 'Login',
 data(){
     return{
-        users:[
-    {
-        login: "admin",
-        password: "admin"
-    },
-    {
-        login: "wojtek",
-        password: "kogut"
-    },
-
-    ],
+        email:'',
+        password:'',
+        path: "",
     }
     
 },
 methods:{
-    // Wylogowanie użytkownika //
-    start(){
-        localStorage.removeItem('user');
-    },
-
-    // walidacja formularza //
-    checkUsers() {
-        const user = {
-            login: $('#inputLog').val(),
-            password: $('#inputPass').val()
-        }
-        const findUser = this.users.find(u => u.login === user.login)
-        if (findUser) {
-            if (user.password === findUser.password) {
-                localStorage.setItem('user', user.login);
-                window.location.href = '#/Header';
-            }
-            else {
-                $('#wrong').html('Niepoprawne haslo')
-            }
-        }
-        else {
-            $('#wrong').html('Niepoprawny login lub haslo')
-        }
-
+    login:function(){
+       firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+           (user) => {
+               this.$router.replace('header')
+             localStorage.setItem("name", user.user.uid)
+                // this.path = user.user.uid
+                
+               
+           },
+          (err) => {
+               alert('Oops. '+ err.message)
+           }
+       )
     }
+},
+beforeDestroy(){
+
 }
+   
+
 }
 </script>
 
 <style scoped>
 
-#wrong{
-    color:red; 
+.login{
+    margin-top: 40px;
+}
+input{
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+}
+button{
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+}
+p{
+    margin-top: 40px;
     font-size: 13px;
-    font-weight: 500;
-}
-.btn-secondary{
-     margin-bottom: 10px;
-}
-.form-group .col-2{
-    margin: 0 auto;
-    min-width: 200px;
-}
-.form-group label{
     color: white;
 }
-form{
-    margin-top: 50px;
-    border: 1px solid gray;
-    border-radius: 5px;
-    padding: 50px 0;
-    background-image: url('../assets/1.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
+p a{
+    text-decoration: underline;
+    cursor: pointer;
 }
-.container{
-    margin-top: 20%;
+h3{
+    color: white;
 }
 
 </style>
