d2bc303360a3259c3b12543d1be439d8eb14f2ad
wkogut93 <w.kogut@ideo.pl>
2019-03-18 09:47:26 +0100

    Update Connect.vue

--- src/components/Connect.vue
+++ src/components/Connect.vue
@@ -14,30 +14,8 @@ data(){
     }
 },
 methods:{
-    connection(){
-        const config = {
-            host     : '55.mysql.ideo',
-            user     : 'root',
-            password : 'root',
-            database : 'praktykant4',
-        }
-        const connection = mysql.createConnection(config);
-
-        connection.connect( err => {
-            if(err) throw err;
-        });
-
-        app.all('/', (req, res)=>{
-            connection.query("Select * from town", (err, result, fields)=>{
-                if (err) throw err;
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(result));
-            });
-            
-        })
-
-        connection.end();
-    }
+ 
+    
 }
 }
 </script>
